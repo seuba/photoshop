3729c1b10a24084dc9113fa547dbcb7d9e54af13
seuba <49449750+seuba@users.noreply.github.com>
2019-06-26 15:07:26 +0200

    Update read.php

--- read.php
+++ read.php
@@ -1,7 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<style>
+body{
+    text-align: center;
+    padding-top: 20px;
+    background: #fbfbfb;
+	font-family:arial;
+	
+}
+.off{
+
+    width: 200px;
+    height: 300px;
+    background: url(house.png);
+    background: url(house.png);
+    background-position: 33px top;
+    background-size: 373px;
+    background-repeat: no-repeat;
+	
+	}
+	
+.on{
+ width: 212px;
+    height: 300px;
+    background: url(house.png);
+    background: url(house.png);
+    background-position: -158px top;
+    background-size: 373px;
+    background-repeat: no-repeat;
+
+}
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 200px;
+  height: 60px;
+}
+
+.switch input { 
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: " OFF";
+  height: 52px;
+  width: 96px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+  TEXT-ALIGN: center;
+  COLOR: #9E9E9E;
+  font-weight: bold;
+    font-family: arial;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+  box-shadow: 0px 1px 36px #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(96px);
+  -ms-transform: translateX(96px);
+  transform: translateX(96px);
+   content: " ON";
+   COLOR:#2196F3;
+ 
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
+</head>
+<body>
 <?php
 $file = fopen("file.txt","r");
 $content = fread($file,filesize("file.txt"));
 fclose($file);
 
 echo $content;
+if ($content == 'a'){
 ?>
+<script>
+$('input[type=checkbox]').trigger('click');
+</script>
+<?php
+}
+
+?>
+<center>
+<div id="bombeta" class="off"></div>
+
+<label class="switch">
+  <input type="checkbox">
+  <span class="slider"></span>
+</label>
+</center>
+<script>
+
+$('input[type=checkbox]').click(function(){
+$("#bombeta").toggleClass('on');
+}); 
+</script>
+
+</body>
+</html> 
