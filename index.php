b93b9a5604aa2a60df3e7d40444b347bbec535a3
seuba <49449750+seuba@users.noreply.github.com>
2019-10-03 17:10:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 $txt = $_GET['var'];
+$txt2 = $_GET['courtid'];
 $myfile = fopen("file.txt", "w") or die("Unable to open file!");
 
 fwrite($myfile, $txt);
-
+fwrite($myfile, $txt2);
 fclose($myfile);
 ?>
