ee3c36d6f4ae77405fe77a6387f2dc35151d4c05
seuba <49449750+seuba@users.noreply.github.com>
2019-10-04 12:19:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-$txt = $_GET['var'];
-$txt2 = $_GET['courtid'];
+$data = json_decode(file_get_contents('php://input'));
+
+	$txt = $data->{'var'};
+$txt2 = $data->{'courtid'};
+
+
+;
 $myfile = fopen("file.txt", "w") or die("Unable to open file!");
 
 fwrite($myfile, $txt);
