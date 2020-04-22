42851df60b0fba4bae2fdaf9b4c5f7924f25a03f
seuba <49449750+seuba@users.noreply.github.com>
2020-04-22 20:53:21 +0200

    Update alexa.php

--- alexa.php
+++ alexa.php
@@ -130,8 +130,13 @@ function GetJsonMessageResponse($RequestMessageType, $EchoReqObj)
 $data = array("var" => "a", "courtid" => "1");                                                                    
 $data_string = json_encode($data);  
 $ch = curl_init('https://fuelseuba.herokuapp.com');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($data_string))                                                                       
+);           
 
 
 $response = curl_exec($ch);
