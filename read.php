9360e13d0afacc316a08a1cd34c381b7866627c6
seuba <49449750+seuba@users.noreply.github.com>
2020-04-23 12:00:31 +0200

    Update read.php

--- read.php
+++ read.php
@@ -147,9 +147,13 @@ var valor = $('#segon').val();
 var targetForm = $('#form');
 var urlWithParams = targetForm.attr('action') + "?" + targetForm.serialize();
 $("#bombeta").toggleClass('on');
-
+var data = '{"valor":" + valor + "courtid":"1"};
 $.ajax({
         url: 'https://fuelseuba.herokuapp.com/?var='+ valor,
+	dataType: 'json',
+        contentType: 'application/json',
+	data: JSON.stringify(data),
+	processData: false,
         success: function(data){
 		
            location.reload();
